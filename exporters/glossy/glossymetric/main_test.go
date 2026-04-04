33db7ecbf12d660395dab3710ba86982b947cfd4
franklin <kevin@franklinkim.de>
2026-04-04 23:29:05 +0200

    refactor: use manual reader for metric reporting

---
+++ exporters/glossy/glossymetric/main_test.go
@@ -0,0 +1,11 @@
+package glossymetric_test
+
+import (
+	"testing"
+
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
--- testing/reportmetrics.go
+++ testing/reportmetrics.go
@@ -2,27 +2,48 @@ package testing
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 	"time"
 
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 )
 
 // TestMainReportMetrics sets up a MeterProvider with a metric exporter for testing and returns it along with a flush function.
 //
-//	func TestMain(m *testing.M) {
-//		mp, flush := oteltesting.TestMainReportMetrics(m, glossymetric.NewTestingM(m))
-//	}
+//		func TestMain(m *testing.M) {
+//	   exporter := glossymetric.NewTestingM(m)
+//			mp, flush := oteltesting.TestMainReportMetrics(m, exporter)
+//		}
 func TestMainReportMetrics(m *testing.M, exporter metric.Exporter) (*metric.MeterProvider, func()) {
-	reader := metric.NewPeriodicReader(exporter, metric.WithInterval(0))
+	reader := metric.NewManualReader()
 	mp := metric.NewMeterProvider(metric.WithReader(reader))
 
-	otel.SetMeterProvider(mp)
+	var pcs [1]uintptr
+
+	n := runtime.Callers(2, pcs[:])
 
 	return mp, func() {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
+
+		var rm metricdata.ResourceMetrics
+		if err := reader.Collect(ctx, &rm); err != nil {
+			panic(err)
+		}
+
+		if n > 0 {
+			f, _ := runtime.CallersFrames([]uintptr{pcs[0]}).Next()
+			_, _ = fmt.Fprintf(os.Stdout, "%s:%d: ", filepath.Base(f.File), f.Line)
+		}
+
+		if err := exporter.Export(ctx, &rm); err != nil {
+			panic(err)
+		}
 
 		if err := mp.Shutdown(ctx); err != nil {
 			panic(err)
@@ -30,33 +51,44 @@ func TestMainReportMetrics(m *testing.M, exporter metric.Exporter) (*metric.Mete
 	}
 }
 
-func ReportMetrics(tb testing.TB, exporter metric.Exporter) (*metric.MeterProvider, func()) {
+// ReportMetrics sets up a MeterProvider for metrics reporting and returns it along with a cleanup function.
+//
+//		func TestWithMetrics(t *testing.T) {
+//	    exporter := glossymetric.NewTest(t)
+//		  mp := oteltesting.ReportMetrics(t, exporter)
+//		}
+func ReportMetrics(tb testing.TB, exporter metric.Exporter) *metric.MeterProvider {
 	tb.Helper()
 
-	reader := metric.NewPeriodicReader(exporter, metric.WithInterval(0))
+	var pcs [1]uintptr
+
+	n := runtime.Callers(2, pcs[:])
+
+	reader := metric.NewManualReader()
 	mp := metric.NewMeterProvider(metric.WithReader(reader))
 
-	prev := otel.GetMeterProvider()
-
-	otel.SetMeterProvider(mp)
-
-	// tb.Cleanup(func() {
-	// 	ctx, cancel := context.WithTimeout(context.WithoutCancel(tb.Context()), time.Second)
-	// 	defer cancel()
-	// 	if err := mp.Shutdown(ctx); err != nil {
-	// 		tb.Fatal(err)
-	// 	}
-	// 	otel.SetMeterProvider(prev)
-	// })
-
-	return mp, func() {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	tb.Cleanup(func() {
+		ctx, cancel := context.WithTimeout(context.WithoutCancel(tb.Context()), time.Second)
 		defer cancel()
+
+		var rm metricdata.ResourceMetrics
+		if err := reader.Collect(ctx, &rm); err != nil {
+			tb.Fatal(err)
+		}
+
+		if n > 0 {
+			f, _ := runtime.CallersFrames([]uintptr{pcs[0]}).Next()
+			_, _ = fmt.Fprintf(tb.Output(), "%s:%d: ", filepath.Base(f.File), f.Line)
+		}
+
+		if err := exporter.Export(ctx, &rm); err != nil {
+			tb.Fatal(err)
+		}
 
 		if err := mp.Shutdown(ctx); err != nil {
 			tb.Fatal(err)
 		}
+	})
 
-		otel.SetMeterProvider(prev)
-	}
+	return mp
 }
